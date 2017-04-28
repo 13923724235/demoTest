09f815127ec0913625843a12df55b76b2f295fe2
wkj <931625530@qq.com>
2017-04-28 09:34:20 +0800

    imageview分类

---
+++ 2017-0427-testUIImageViewLoadImage/2017-0427-testUIImageViewLoadImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2017-0427-testUIImageViewLoadImage
+//
+//  Created by 007 on 2017/4/27.
+//  Copyright © 2017年 wkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
